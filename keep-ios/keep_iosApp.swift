ca62933891b44df782a87feffa80b6edb93aaec0
Kevin Griffin <griffin.kev@gmail.com>
2022-03-09 11:32:53 -0500

    Initial Commit

---
+++ keep-ios/keep_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  keep_iosApp.swift
+//  keep-ios
+//
+//  Created by Kevin Griffin on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct keep_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
